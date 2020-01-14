9c70ab16f0e80a96f881f7b323e321dfa9efb290
Angelo Crincoli <angelocrincoli91@gmail.com>
2020-01-14 16:55:56 -0400

    Booking my app

---
+++ users/routes/users.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const userRouter = express.Router();
+const auth = require("../auth/auth");
+const User = require('../models/user');
+
+//**************************** USER CRUD************************************//
+userRouter.post('/', async (req, res) => {
+	try {
+
+		const user = {
+			usuario: req.body.usuario,
+			email: req.body.correo,
+			contraseña: req.body.contraseña,
+			tipo: req.body.tipo,
+		};
+
+		let response = await User.addUser(user);
+		res.status(200).json(response);
+	}
+	catch (e) {
+		res.status(400).json(e.toString());
+	}
+});
+
+
+// Delete user
+userRouter.delete('/', auth, async (req, res, next) => {
+	try {
+
+		const item = req.query.item;
+		let response = await User.deleteUser(item.usuario);
+		res.status(200).json(response);
+	}
+	catch (e) {
+		res.status(400).json(e.toString());
+	}
+});
+
+// Update user, NEED TO IMPROVE
+userRouter.put('/', auth, async (req, res, next) => {
+	try {
+		const user = req.user;
+		const updateData = req.body.updateData;
+
+		let response = await User.updateUser(updateData);
+		res.status(200).json(response);
+	}
+	catch (e) {
+		res.status(400).json(e.toString());
+	}
+
+
+});
+
+// Get User
+userRouter.get('/', auth, async (req, res, next) => {
+	return res.json({
+		user: req.user
+	});
+});
+
+
+userRouter.get('/all', auth, async (req, res, next) => {
+	try {
+
+		let response = await User.getUsers();
+		res.status(200).json(response);
+	}
+	catch (e) {
+		res.status(400).json(e.toString());
+	}
+
+});
+
+module.exports = userRouter;
